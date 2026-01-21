2516a32b094166c32bab80cde3ae6788c706fa31
1993bincy <bicybj93@gmail.com>
2026-01-21 11:08:03 +0530

    first commit

---
+++ src/org/emp/Employee.java
@@ -0,0 +1,45 @@
+package org.emp;
+
+public class Employee {
+	
+ public void empID () {
+	 System.out.println("12345");}
+ 
+	 public void empName() {
+	
+	 System.out.println("Aadith");}
+
+	public void empDob() {
+	System.out.println("09-11-2022");}
+	
+	public void empPhone() {
+	System.out.println("1234566");}
+	
+	public void empEmail(){
+System.out.println("abc@gmail.com");}
+
+public void empAddress() {
+System.out.println("Thillainagar");}
+public static void main (String[]args) {
+	Employee ee = new Employee();
+	ee.empID();
+	ee.empName();
+	ee.empDob();
+	ee.empPhone();
+	ee.empAddress();
+	ee.empEmail();
+	
+
+		 
+		 
+		 
+		 
+		 
+	 }
+	 
+	
+	
+}
+	
+	
+
